8b35ea74f18cfc5d13ec481ccb0631724351f42a
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-10-12 07:31:15 -0300

    ci: fixing previous fail

--- math_test.go
+++ math_test.go
@@ -10,26 +10,26 @@ func TestSoma(t *testing.T) {
 	}
 }
 
-//func TestSubtracao(t *testing.T) {
-//	total := Subtracao(15, 15)
-//
-//	if total != 0 {
-//		t.Errorf("Resultado da subtração é inválido: Resultado %d. Esperado: %d", total, 0)
-//	}
-//}
-//
-//func TestMultiplicacao(t *testing.T) {
-//	total := Multiplicacao(15, 15)
-//
-//	if total != 225 {
-//		t.Errorf("Resultado da multiplicação é inválido: Resultado %d. Esperado: %d", total, 225)
-//	}
-//}
-//
-//func TestDivisao(t *testing.T) {
-//	total := Divisao(15, 15)
-//
-//	if total != 1 {
-//		t.Errorf("Resultado da divisão é inválido: Resultado %d. Esperado: %d", total, 1)
-//	}
-//}
+func TestSubtracao(t *testing.T) {
+	total := Subtracao(15, 15)
+	
+	if total != 0 {
+		t.Errorf("Resultado da subtração é inválido: Resultado %d. Esperado: %d", total, 0)
+	}
+}
+
+func TestMultiplicacao(t *testing.T) {
+	total := Multiplicacao(15, 15)
+	
+	if total != 225 {
+		t.Errorf("Resultado da multiplicação é inválido: Resultado %d. Esperado: %d", total, 225)
+	}
+}
+
+func TestDivisao(t *testing.T) {
+	total := Divisao(15, 15)
+	
+	if total != 1 {
+		t.Errorf("Resultado da divisão é inválido: Resultado %d. Esperado: %d", total, 1)
+	}
+}
